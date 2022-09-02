3c7794e970b6485e9b153ff966d3f50a83e529a2
shankart24 <itsshankart44@gmail.com>
2022-09-02 20:39:47 +0530

    Dynamically fetching options & basic search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,130 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
+    const [state, setState] = useState({
+        searchText: "",
+        selectedColors: [],
+        selectedShapes: [],
+        selectedSizes: [],
+        finalData: [],
+        shapeOptions: [],
+        colorOptions: [],
+        sizeOptions: []
+    });
+
+    async function fetchData() {
+        const finalUrl = generateUrl();
+        const res = await fetch(finalUrl);
+        const data = await res.json();
+        setState(state => ({ ...state, finalData: data }));
+    }
+
+    function generateUrl() {
+        let url = `http://localhost:3000/planets?q=${state.searchText}`;
+        const { selectedColors, selectedShapes, selectedSizes } = state;
+        selectedColors?.forEach(color => (url += `&color=${color}`));
+        selectedShapes?.forEach(shape => (url += `&shape=${shape}`));
+        selectedSizes?.forEach(size => (url += `&size=${size}`));
+        return url;
+    }
+
+    async function allOptions(type) {
+        const url = `http://localhost:3000/${type}s`;
+        const res = await fetch(url);
+        const data = await res.json();
+        setState(state => ({ ...state, [`${type}Options`]: data }));
+    }
+
+    useEffect(() => {
+        allOptions("color");
+        allOptions("shape");
+        allOptions("size");
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [state.selectedColors, state.selectedShapes, state.selectedSizes]);
+
     return (
         <section>
-            <h1 className="text-blue-900 font-bold text-7xl tracking-tighter">
-                Hello there
-            </h1>
+            <input
+                className="border border-gray-400 m-6"
+                type="text"
+                value={state.searchText}
+                onChange={e =>
+                    setState(state => ({
+                        ...state,
+                        searchText: e.target.value
+                    }))
+                }
+            />
+            <button
+                className="bg-blue-900 text-white"
+                onClick={() => {
+                    fetchData();
+                }}
+            >
+                Search
+            </button>
+            {state.colorOptions?.length !== 0 &&
+                state.sizeOptions?.length !== 0 &&
+                state.shapeOptions?.length !== 0 &&
+                [
+                    { label: "Shape", data: state.shapeOptions },
+                    { label: "Color", data: state.colorOptions },
+                    { label: "Size", data: state.sizeOptions }
+                ]?.map(section => {
+                    return (
+                        <div key={section.label}>
+                            <h3>{section.label}</h3>
+                            {section.data?.map(item => {
+                                return (
+                                    <label
+                                        key={item.id}
+                                        className="inline-flex items-center"
+                                    >
+                                        <input
+                                            type="checkbox"
+                                            name="shapes"
+                                            value={item.id}
+                                            onChange={e => {
+                                                const val = e.target.value;
+                                                setState(state => ({
+                                                    ...state,
+                                                    selectedColors:
+                                                        !state.selectedColors?.includes(
+                                                            val
+                                                        )
+                                                            ? [
+                                                                  ...state.selectedColors,
+                                                                  val
+                                                              ]
+                                                            : state.selectedColors?.filter(
+                                                                  item =>
+                                                                      item !==
+                                                                      val
+                                                              )
+                                                }));
+                                            }}
+                                            className="h-4 w-4 text-gray-900"
+                                        />
+                                        <span className="ml-2 mb-1 text-sm  text-gray-700">
+                                            {item.name}
+                                        </span>
+                                    </label>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+
+            {state.finalData?.map(item => {
+                return (
+                    <li key={item.id}>
+                        {item.name} <br />
+                    </li>
+                );
+            })}
         </section>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { myStore } from "./store";
+import { myStore } from "./Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
