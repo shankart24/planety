11c7f78f3c2f265d2200b1cd48d787b24af0526d
shankart24 <itsshankart44@gmail.com>
2022-09-02 18:48:30 +0530

    Basic redux store setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
 export default function App() {
-	return <h1 className="text-blue-900 font-bold text-7xl tracking-tighter">Hello there</h1>;
+    return (
+        <section>
+            <h1 className="text-blue-900 font-bold text-7xl tracking-tighter">
+                Hello there
+            </h1>
+        </section>
+    );
 }
